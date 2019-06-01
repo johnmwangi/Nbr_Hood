1fc3cfc70a23287bfeb5fd5a6c4102550eeb78bd
johnmwangi <jonesmwas356@gmail.com>
2019-06-01 14:56:08 +0300

    some styles added to the css

--- nbr/views.py
+++ nbr/views.py
@@ -181,7 +181,7 @@ def create_hood(request):
         return redirect('home')
     else:
         form = CreateHoodForm()
-    return render(request, 'hood/create_hood.html', {"form": form})
+    return render(request, 'hoods/create_hood.html', {"form": form})
 
 
 @login_required(login_url='/accounts/login/')
