c176cfd50b15009679d339515b3b2cfc24248157
johnmwangi <jonesmwas356@gmail.com>
2019-06-01 22:05:35 +0300

    links added to the urls

--- nbr/urls.py
+++ nbr/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
