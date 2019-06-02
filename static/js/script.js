447ac25e39d034073e4dd9bd24dee9ab72f7aebf
johnmwangi <jonesmwas356@gmail.com>
2019-06-02 13:04:11 +0300

    addition commited to the index file

---
+++ static/js/script.js
@@ -0,0 +1,3 @@
+background-image: url(https://images.pexels.com/photos/1308624/pexels-photo-1308624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)
+}
+https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940
